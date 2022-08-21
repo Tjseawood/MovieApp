38a5708fe60e8d20ef5f94f808bb226977f6ad73
T.Seawood <62930969+Tjseawood@users.noreply.github.com>
2022-08-21 16:26:37 -0700

    initial vite template

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+import { apiKey } from './secrets.js'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
+console.log(apiKey)
+
+
