e7fe476288dfbe88ef33e93479e958a5e13a522c
T.Seawood <62930969+Tjseawood@users.noreply.github.com>
2022-08-22 16:27:34 -0700

    Updated the form in HTML & Core Functions in JS

--- main.js
+++ main.js
@@ -4,55 +4,27 @@ console.log(apiKey)
 
 
 //func 1 - get form values
-const getFormValues = () => {
-   //Get userInput from form
+const getUserInput = () => {
   const movieId = document.getElementById('movie-id')
-  const movieTitle = document.getElementById('title')
-  const movieYear = document.getElementById('year')
-  const moviePlot = document.getElementById('plot')
-  const apiKey = document.getElementById('apiKey')
-  }
+  const movieApi = document.getElementById('movieApiKey')
+  const movieTitle = document.getElementById('movieTitle')
+  const movieYr = document.getElementById('movieYear')
+  const moviePlot = document.getElementById('moviePlot')
+  
+}
+console.log(getFormValues)
 
 //func 2 - Call API with user input for results, take in form values 
 
-const getResults = () => {
-  const mockResults = {
-    Title: "James Bond",
-    Year: "2015",
-    Rated: "N/A",
-    Released: "24 Jul 2015",
-    Runtime: "142 min",
-    Genre: "Action, Comedy",
-    Director: "Sai Kishore Macha",
-    Writer: "Shyam Ch, Gopimohan, Sai Kishore Macha",
-    Actors: "Allari Naresh, Sakshi Chaudhary, Raghu Babu",
-    Plot: "A down-to-earth software employee marries a lady without knowing about her past and criminal activities. Halfway through his marriage, he finds out her background and decides to run away, but ultimately comes to terms with his pro...",
-    Language: "Telugu",
-    Country: "India",
-    Awards: "N/A",
-    Poster: "https://m.media-amazon.com/images/M/MV5BM2IwZDRiOTAtNTUxOC00ZWQxLThlYTYtZTM2NTRmMjEzNmQwXkEyXkFqcGdeQXVyODAzNzAwOTU@._V1_SX300.jpg",
-    Ratings: [
-    {
-    Source: "Internet Movie Database",
-    Value: "5.5/10"
-    }
-    ],
-    Metascore: "N/A",
-    imdbRating: "5.5",
-    imdbVotes: "241",
-    imdbID: "tt4896340",
-    Type: "movie",
-    DVD: "02 Aug 2018",
-    BoxOffice: "N/A",
-    Production: "N/A",
-    Website: "N/A",
-    Response: "True"
-  }
-  return mockResults
+async function makeApiCall(userInputs) {
+  const urlToFetch = getUserInput
+  const response = await fetch(urlToFetch)
+  const apiResults = await response.json()
+  return apiResults
 }
 
 
-// //func 3 - Show API results back to user, take end results and show them 
+//func 3 - Show API results back to user, take end results and show them 
 const showResults = (results) => {
   console.log(results)
   //Results in HTML
@@ -65,4 +37,5 @@ const main = () => {
   showResults(apiResults)
 }
 
-// main()
+
+main()
