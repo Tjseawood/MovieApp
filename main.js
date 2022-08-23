500f5461af2fed21972614c2a251318c212c879f
T.Seawood <62930969+Tjseawood@users.noreply.github.com>
2022-08-23 14:27:38 -0700

    refactored main.js into single function

--- main.js
+++ main.js
@@ -3,48 +3,29 @@ import { apiKey } from './secrets.js'
 // console.log(apiKey)
 
 
-//func 1 - get form values
-const getUserInputs = () => {
-  // const movieId = document.getElementById('movie-id')
-  // const movieApi = document.getElementById('movieApiKey')
-  const movieTitle = document.getElementById('movieTitle').value 
-  // const movieYr = document.getElementById('movieYear')
-  const moviePlot = document.getElementById('moviePlot').value
-  return {
-    'Title': movieTitle,
-    'Plot': moviePlot,
-  }
-}
-// console.log(getUserInputs())
-const generateUrl = () => {
- return 'http://www.omdbapi.com/?i=tt3896198&apikey=YOURKEYHERE'
-}
-// func 2 - Call API with user input for results, take in form values 
-//create app url for userinput vaules
-async function makeApiCall(x) {
-  const urlToFetch = generateUrl(x)
-  const response = await fetch(urlToFetch)
-  const apiResults = await response.json()
-  return apiResults
-}
-
-//func 3 - Show API results back to user, take end results and show them 
-const showResultsFromApiCall = (results) => {
-  console.log(results)
-  //Results in HTML
-}
-
 const submitForm = async (event) => {
   event.preventDefault() //override action in standard html form
-  const userInputs = getUserInputs()
-  console.log(userInputs)
-  const apiResults = await makeApiCall(userInputs)
-  showResultsFromApiCall(apiResults)
+  const movieTitle = document.getElementById('movieTitle').value 
+  const moviePlot = document.getElementById('moviePlot').value
+  // const userInputs = {
+  //   'Title': movieTitle,
+  //   'Plot': moviePlot,
+  // }
+  console.log(movieTitle, moviePlot)
+  //Todo: Define generateUrl eventually
+  const urlToFetch = `http://www.omdbapi.com/?t=${movieTitle}&plot=${moviePlot}&apikey=${apiKey}`
+  console.log(urlToFetch)
+  const response = await fetch(urlToFetch)
+  const apiResults = await response.json()
+
+  console.log(apiResults)
+ 
+
 }
 
-const main = () => {
-  document.getElementById('searchForm').addEventListener('submit', submitForm)
+const main = async () => {
+  const searchFormElement = document.getElementById('searchForm')
+    searchFormElement.addEventListener('submit', submitForm)
 }
-
 
 main()
