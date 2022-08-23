72542f9baf847cc366618f9c4414b26c8d854012
T.Seawood <62930969+Tjseawood@users.noreply.github.com>
2022-08-23 16:28:28 -0700

    refactored HTML and replaced API parameter from 't' to 's' so user can search multiple versions of the intented  Movie

--- main.js
+++ main.js
@@ -2,39 +2,42 @@ import './style.css'
 import { apiKey } from './secrets.js'
 // console.log(apiKey)
 
-const submitForm = async (event) => {
+const loadMovies = async (event) => {
   event.preventDefault() //override action in standard html form
   const movieTitle = document.getElementById('movieTitle').value 
   const moviePlot = document.getElementById('moviePlot').value
-  // const userInputs = {
-  //   'Title': movieTitle,
-  //   'Plot': moviePlot,
-  // }
-  console.log(movieTitle, moviePlot)
-  //Todo: Define generateUrl eventually
-  const urlToFetch = `https://www.omdbapi.com/?t=${movieTitle}&plot=${moviePlot}&apikey=${apiKey}`
-  console.log(urlToFetch)
-  const response = await fetch(urlToFetch)
-  const apiResults = await response.json()
+  // console.log(movieTitle, moviePlot)
+  const urlToFetch = `https://www.omdbapi.com/?s=${movieTitle}&plot=${moviePlot}&apikey=${apiKey}`
+  // console.log(urlToFetch)
+  const response = await fetch(urlToFetch);
+  const loadMovies = await response.json();
 
-  console.log(apiResults)
+  console.log(loadMovies.Search)
 
-  const movieDetails = document.createElement('h1')
-  movieDetails.textContent = apiResults['Title'];
+  // const t = document.createElement('h1')
+  // t.textContent = apiResults['Title'];
+
+  // const p = document.createElement('p')
+  // p.textContent = apiResults['Plot']
+
+  // const ps = document.createElement('img')
+  // ps.textContent = apiResults['Poster']
+
+  // const r = document.createElement('p')
+  // r.textContent = apiResults['Rated']
+
+  // const showMovieResults = document.getElementById('showMovieResults')
+
+  // showMovieResults.appendChild(t)
+  // showMovieResults.appendChild(p)
+  // showMovieResults.appendChild(r)
+  // showMovieResults.appendChild(ps)
   
-  const returnMoviePlot = document.createElement('p')
-  movieDetails.textContent = apiResults['Plot'];
-
-  const parent = document.getElementById('results')
-  parent.appendChild(movieDetails)
-
-  parent.document.getElementById('results')
-  parent.appendChild(movieDetails)
 }
 
 const main = async () => {
   const searchFormElement = document.getElementById('searchForm')
-    searchFormElement.addEventListener('submit', submitForm)
+    searchFormElement.addEventListener('submit', loadMovies)
 }
 
 main()
