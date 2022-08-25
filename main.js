5682fc3894e78db14e0ab65bd9a17a4edfe7fff9
T.Seawood <62930969+Tjseawood@users.noreply.github.com>
2022-08-25 00:44:45 -0700

    Renamed Project Title, Updated CSS, and Updated JS

--- main.js
+++ main.js
@@ -26,41 +26,61 @@ const loadMovies = async (event) => {
   //Append Results to HTML
   function displayMovieDetails() {
     const parent = document.getElementById('movieResults')
+
+    const poster = document.createElement('img')
+    poster.src = apiResults['Poster']
+    poster.className = "poster-img"
+    parent.appendChild(poster);
     
     const title = document.createElement('div')
     title.textContent = apiResults['Title'];
     title.className = "movie-title"
     parent.appendChild(title)
     
-    const movieyr = document.createElement('p')
-    movieyr.textContent = `Year: ${apiResults['Year']}`;
-    movieyr.className = "year"
-    parent.appendChild(movieyr)
-
     const movieId = document.createElement('div')
     movieId.textContent = `imdbID: ${apiResults['imdbID']}`;
-    movieId.className = "movie-id"
+    movieId.className = "movie-main-details"
+    movieId.id ="movie-id"
     parent.appendChild(movieId)
+
+    const movieyr = document.createElement('div')
+    movieyr.textContent = `Year: ${apiResults['Year']}`;
+    movieyr.className = "movie-main-details"
+    movieyr.id ="year"
+    parent.appendChild(movieyr)  
+
+    const rating = document.createElement('div')
+    rating.textContent = `Rated: ${apiResults['Rated']}`;
+    rating.className = "movie-main-details"
+    rating.id = "rated"
+    parent.appendChild(rating)
+        
+    const releaseDate = document.createElement('div')
+    releaseDate.textContent = `Released: ${apiResults['Released']}`;
+    releaseDate.className = "movie-main-details"
+    releaseDate.id ="released"
+    parent.appendChild(releaseDate)
+
+    const actors = document.createElement('div')
+    actors.textContent = `Actors: ${apiResults['Actors']}`;
+    actors.className = "movie-main-details"
+    actors.id ="actors"
+    parent.appendChild(actors)
 
     const plot = document.createElement('div')
     plot.textContent = `Plot: ${apiResults['Plot']}`;
     plot.className = "plot"
     parent.appendChild(plot)
    
-    const rating = document.createElement('div')
-    rating.textContent = `Rated: ${apiResults['Rated']}`;
-    rating.className = "rated"
-    parent.appendChild(rating)
-    
-    const releaseDate = document.createElement('div')
-    releaseDate.textContent = `Released: ${apiResults['Released']}`;
-    releaseDate.className = "released"
-    parent.appendChild(releaseDate)
-    
-    const poster = document.createElement('img')
-    poster.src = apiResults['Poster']
-    poster.className = "poster-img"
-    parent.appendChild(poster);
+    const btn = document.getElementById("button")
+    btn.addEventListener('click', function handleClick(event) {
+      event.preventDefault()
+      const inputs = document.querySelectorAll('#movieApi, #movieTitle, #moviePlot')
+      inputs.forEach(input => {
+        input.value = '';
+      });
+    });
+   
   }
 } 
 
